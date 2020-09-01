9fc0d565ba4862833fe5fc00b89a22788d8aa2e0
Blake <bryantb2@my.lanecc.edu>
2020-08-31 17:59:42 -0700

    fixed compatibility errors with Chrome

--- src/styleConstants/index.js
+++ src/styleConstants/index.js
@@ -48,13 +48,13 @@ const size = {
 };
 
 const devices = {
-    mobileS: `(min-width: ${size.mobileS})`,
-    mobileM: `(min-width: ${size.mobileM})`,
-    mobileL: `(min-width: ${size.mobileL})`,
-    tablet: `(min-width: ${size.tablet})`,
-    laptop: `(min-width: ${size.laptop})`,
-    laptopL: `(min-width: ${size.laptopL})`,
-    desktop: `(min-width: ${size.desktop})`
+    mobileS: `min-width: ${size.mobileS}`,
+    mobileM: `min-width: ${size.mobileM}`,
+    mobileL: `min-width: ${size.mobileL}`,
+    tablet: `min-width: ${size.tablet}`,
+    laptop: `min-width: ${size.laptop}`,
+    laptopL: `min-width: ${size.laptopL}`,
+    desktop: `min-width: ${size.desktop}`
 };
 
 export const theme = {
